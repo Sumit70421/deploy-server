9e6c9688ccf451e0dcda80e37c923a078c04e06c
Sumit- Patil <69342422+Sumit70421@users.noreply.github.com>
2021-11-13 18:36:17 +0530

    Integrated with front end

--- src/server.js
+++ src/server.js
@@ -1,41 +1,49 @@
 const express = require("express");
+var cors = require('cors')
 const app = express();
 const serverless = require("serverless-http");
 // const mongoose = require("mongoose");
 const {MongoClient} = require('mongodb');
 const mongoose  = require("mongoose");
+const { data } = require("cheerio/lib/api/attributes");
 const router = express.Router();
-const url = "mongodb+srv://rohini:p_Tp6Yf%40wSJ%40PRP@inshorts-news-scraper.s3hct.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.use(cors())
+const url = "mongodb+srv://rohini:p_Tp6Yf%40wSJ%40PRP@inshorts-news-scraper.s3hct.mongodb.net/db?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser : true , useUnifiedTopology : true},
     (err , connection)=>{
         if (err) console.log(err);
-        else   console.log(connection.Mongoose)
+        else   console.log("connected")
 })
 const newsSchema = mongoose.Schema({
-    
+    index:Number,
+    news_title:String,
+    news_content:String,
+    news_image:String,
+    news_url:String
 })
 
 const NewsDB = mongoose.model("NewsDB",newsSchema);
 
-// const apple = new Product({
-//     imgUrl : "https://i.ibb.co/Gvg71g6/s-l640.jpg",
-//     productName : "AirPods",
-//     rating : 4,
-//     price : 200,
-//     type: "Accessories"
+// const apple = new NewsDB({
+//     index: 40 ,
+//     news_url : "https://i.ibb.co/Gvg71g6/s-l640.jpg",
+//     news_title : "AirPods",
+//     news_image : "https://i.ibb.co/Gvg71g6/s-l640.jpg",
+//     news_content: "Accessories"
 // })
 
 // apple.save((err, result)=>{
 //     if (err) console.log(err);
 //     else console.log(result);
 // })
-
+var data1 = [];
 NewsDB.find((err,result)=>{
     if (err) {console.log("nhi hora");}
     else {
         // router.get("/",(req,res)=>{
             // res.json(result)
-            console.log(result)
+            console.log(result);
+            data1 = result;
         // });
         // const jsonSO = ["ahppy","sad"];
         // result.forEach((document)=>{
@@ -45,10 +53,13 @@ NewsDB.find((err,result)=>{
     } 
 })
 
+app.get('/', (req, res) => {
+    res.json(data1)
+  })
 
-app.use(`/`, router);
 
-
-
+app.listen(4000, () => {
+    console.log(`This app listening at http://localhost:4000`)
+  })
 module.exports = app;
 
